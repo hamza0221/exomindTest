0ba7f87063ff30d27060f0826c5c6f577b341c12
hamza0221 <hamza.horry@gmail.com>
2022-11-23 00:38:20 +0100

    add api repository

--- lib/model/weather.dart
+++ lib/model/weather.dart
@@ -1,11 +1,24 @@
+import 'package:exomind_test/utils/maths.dart';
+
 class Weather {
-  String? main;
+  String? dateTime;
+  String? temp;
   String? icon;
-  double? temp;
+  String? description;
 
-  Weather(this.main, this.icon, this.temp);
+  Weather({
+    this.dateTime,
+    this.temp,
+    this.icon,
+    this.description,
+  });
 
-  Weather.fromJson(Map<dynamic, dynamic> json) {}
-
-  Map<String, dynamic> toJson() => {"main": main, "icon": icon, "temp": temp};
+  Weather.fromJson(Map<dynamic, dynamic> json) {
+    dateTime = json["dt_txt"];
+    //converting from Kelvin to Celsius ( -273.15)
+    //round the double (two after DECIMAL)
+    temp = convertTemperature(json['main']['temp']).toStringAsFixed(2);
+    icon = (json['weather'][0])['icon'];
+    description = (json['weather'][0])['description'];
+  }
 }
---
+++ lib/utils/maths.dart
@@ -0,0 +1,5 @@
+import 'package:exomind_test/utils/global.dart';
+
+double convertTemperature(double temp) {
+  return temp - kelvin;
+}
